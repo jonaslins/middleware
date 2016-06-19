9665bb5a94600002564b7501dbc72b76239ec075
rayana alencar <rvsa2@cin.ufpe.br>
2016-06-19 17:39:56 -0300

    Subscriber

--- src/jms/Subscriber.java
+++ src/jms/Subscriber.java
@@ -1,5 +1,26 @@
 package jms;
 
 public class Subscriber {
-
+	private int port;
+	private String ip;
+	
+	public Subscriber(int port, String ip) {
+		super();
+		this.port = port;
+		this.ip = ip;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	
+	
 }
